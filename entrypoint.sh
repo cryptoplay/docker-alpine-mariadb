39cc97b9120bb0d1e8214289b2c500da0a0b5504
Vincent Boutour <vincent.boutour@gmail.com>
2016-01-05 16:17:30 +0100

    fixing entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
-set -e
 
-if [ "${1:0:1}" = '-' ]; then
-  set -- mysqld "$@"
-fi
-
-if [ "$1" = 'mysqld' ]; then
-  if [ ! -d "${DATADIR}/mysql" ]; then
-    if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
+if [[ ${1} = 'mysqld' ]]; then
+  if [[ ! -d ${MYSQL_DIR} ]]; then
+    if [[ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]]; then
       echo >&2 'error: database is uninitialized and MYSQL_ROOT_PASSWORD not set'
       echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
       exit 1
     fi
 
     echo 'Initializing database'
-    mysql_install_db --ldata="${DATADIR}" --basedir=/usr/ --user=mysql
+    mysql_install_db --ldata=${MYSQL_DIR} --basedir=/usr/ --user=mysql
     echo 'Database initialized'
 
     tempSqlFile='/tmp/mysql-first-time.sql'
@@ -25,14 +20,14 @@ if [ "$1" = 'mysqld' ]; then
       DROP DATABASE IF EXISTS test ;
 EOSQL
     
-    if [ "${MYSQL_DATABASE}" ]; then
+    if [[ ! -z ${MYSQL_DATABASE} ]]; then
       echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` ;" >> "$tempSqlFile"
     fi
     
-    if [ "${MYSQL_USER}" -a "${MYSQL_PASSWORD}" ]; then
+    if [[ ! -z ${MYSQL_USER} -a ! -z ${MYSQL_PASSWORD} ]]; then
       echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;" >> "$tempSqlFile"
       
-      if [ "${MYSQL_DATABASE}" ]; then
+      if [[ ! -z ${MYSQL_DATABASE} ]]; then
         echo "GRANT ALL ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' ;" >> "$tempSqlFile"
       fi
     fi
